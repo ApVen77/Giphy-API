9087e9a62fe1598402ac67205154bd817aace326
April Long <april11dance@gmail.com>
2019-09-18 22:05:31 -0600

    testing buttons

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -8,22 +8,26 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script type="text/javascript"></script> 
     $("button").on("click", function() {
-        var rapper= $(this).attr("data-rapper")
-        var queryKey="https://api.giphy.com/v1/gifs/search?q=" + rapper
+        var rapper= $(this).attr("data-rapper");
+        var queryURL="https://api.giphy.com/v1/gifs/search?q=" + rapper
         + "&api_key=7s9K1paRvtAMBtvm1uOsui9v56po5P61&limit5";
          
         //AJAX GET request to our queryURL//
         ajax ({
-            url: queryKey,
-            method: 'GET'
+            url: queryURL,
+            method: "GET"
         })
-
-}).then(function(response) {
+.then(function(response) {
+    var requests= response.data;
     console.log(response)
 });
 
-$("button").on(click, function()){
-    var rapper = $(this).attr("data-rapper")
-    var queryURL 
+//create a loop
+
+for (var i=0; i< requests.length; i==)
+{
+
+
 }
+
 
